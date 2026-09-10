8934c12d1877f9d30005b9c7a0de3d283fe48db7
Aayush joshi <100678671+Aayushjoshi12@users.noreply.github.com>
2026-09-11 01:20:35 +0545

    Fix Windows renderer header compile issues

--- src/Frontend/headerfiles/RendererInterface.h
+++ src/Frontend/headerfiles/RendererInterface.h
@@ -1,7 +1,14 @@
 #pragma once
+#ifndef NOGDI
+#define NOGDI
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <iostream>
 #include <raylib.h>
 #include <string>
+#include <vector>
 #include "../../Backend/headerfiles/entities.h"
 using namespace std;
 
@@ -25,12 +32,13 @@ public:
     void drawClock(float currentTime, float totalTime, int x, int y, int radius);
     void drawBackground();
     void drawWholeInterface(Card *hand, int count,int round);
-    void drawPlayedCards(vector<Move> &moves,int x,int y);
+    void drawPlayedCards(std::vector<Move> &moves,int x,int y);
     void drawMute(float x, float y);
-    void drawWholeInterface(Card* hand, int count,Rectangle* rects,vector<Move> moves,float time,int round);
+    void drawWholeInterface(Card* hand, int count,Rectangle* rects,std::vector<Move> moves,float time,int round);
     void drawWholeInterface(Card* hand, int count, int round, const std::string (&labels)[4]);
-    void drawWholeInterface(Card* hand, int count, Rectangle* rects, vector<Move> moves, float time, int round, const std::string (&labels)[4]);    void drawWholeInterface(Card* hand, int count, int round, const std::string (&labels)[4], int currentTurnPlayerId, int localPlayerId);
-    void drawWholeInterface(Card* hand, int count, Rectangle* rects, vector<Move> moves, float time, int round, const std::string (&labels)[4], int currentTurnPlayerId, int localPlayerId);
-    // NEW: also shows each seat's current bid and tricks won, live, during play
-    void drawWholeInterface(Card* hand, int count, Rectangle* rects, vector<Move> moves, float time, int round, const std::string (&labels)[4], int currentTurnPlayerId, int localPlayerId, const int (&bids)[4], const int (&tricksWon)[4]);
+    void drawWholeInterface(Card* hand, int count, Rectangle* rects, std::vector<Move> moves, float time, int round, const std::string (&labels)[4]);
+    void drawWholeInterface(Card* hand, int count, int round, const std::string (&labels)[4], int currentTurnPlayerId, int localPlayerId);
+    void drawWholeInterface(Card* hand, int count, Rectangle* rects, std::vector<Move> moves, float time, int round, const std::string (&labels)[4], int currentTurnPlayerId, int localPlayerId);
+    // Also shows each seat's current bid and tricks won, live, during play
+    void drawWholeInterface(Card* hand, int count, Rectangle* rects, std::vector<Move> moves, float time, int round, const std::string (&labels)[4], int currentTurnPlayerId, int localPlayerId, const int (&bids)[4], const int (&tricksWon)[4]);
 };
